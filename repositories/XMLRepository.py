1fc88e5453d0c1316ef74b0dc133cd292ef8f89f
merelaine <chms2603@gmail.com>
2024-04-11 17:04:02 +0700

    lab4

--- repositories/XMLRepository.py
+++ repositories/XMLRepository.py
@@ -8,14 +8,35 @@ class XMLRepository(Repository):
         self.name = name
         self.path = path
 
-    def GetFullPath(self):
-        return self.path + f"\\{self.name}"
+    def get_full_path(self):
+        return os.path.join(self.path, self.name)
+
+    def get_root(self):
+        try:
+            tree = ET.parse(self.get_full_path())
+            root = tree.getroot()
+        except FileNotFoundError:
+            return 'File was not found'
+
+        return root
 
     def add(self, item):
-        pass
+        root = self.get_root()
+
+        items = self.get_all()
+        items.append(item)
+
+        tree = ET.ElementTree(root)
+        tree.write(self.get_full_path())
 
     def remove(self, item):
-        pass
+        root = self.get_root()
+
+        items = self.get_all()
+        items = [i for i in items if i.id != item.id]
+
+        tree = ET.ElementTree(root)
+        tree.write(self.get_full_path())
 
     def get(self, id):
         items = self.get_all()
@@ -25,7 +46,40 @@ class XMLRepository(Repository):
                 return item
 
     def get_all(self):
-        pass
+        items = []
+        tree = ET.parse(self.get_full_path())
+        root = tree.getroot()
+        for elem in root.findall("./*"):
+            item = self.create_item_from_element(elem)
+            items.append(item)
+
+        return items
 
     def update(self, item):
-        pass
+        root = self.get_root()
+
+        items = self.get_all()
+        for i, current_item in enumerate(items):
+            if current_item.id == item.id:
+                items[i] = item
+            break
+        tree = ET.ElementTree(root)
+        tree.write(self.get_full_path())
+
+    def create_item_from_element(self, elem):
+        if elem.tag == 'course':
+            id = int(elem.get('id'))
+            name = elem.find('name').text
+            description = elem.find('description').text
+            duration = int(elem.find('duration').text)
+            price = float(elem.find('price').text)
+            teacher = elem.find('teacher').text
+            students = []
+            students = []
+            for student_elem in elem.findall('student'):
+                student_id = int(student_elem.get('id'))
+                student_name = student_elem.find('name').text
+
+                student = Student(student_id, student_name)
+                students.append(student)
+            return Course(id, name, description, duration, price, teacher)
--- use_case_layer/CourseService.py
+++ use_case_layer/CourseService.py
@@ -7,7 +7,7 @@ class CourseService:
         self.stud_repo = stud_repo
         self.teacher_repo = teacher_repo
 
-    def create_course(self,name: str, description:str, duration:int,price:float,teacher):
+    def create_course(self, name: str, description: str, duration: int, price: float, course_teacher):
         courses = self.course_repo.get_all()
 
         if len(courses) > 0:
@@ -15,29 +15,29 @@ class CourseService:
         else:
             last_id = 0
 
-        if isinstance(teacher,Teacher):
-            teacher_id = teacher.id
-        elif teacher is None:
+        if isinstance(course_teacher,Teacher):
+            teacher_id = course_teacher.id
+        elif course_teacher is None:
             teacher_id = None
         else:
             return 'Set teacher or None'
         new_course = Course(last_id,name,description,duration,price,teacher_id,[])
         self.course_repo.add(new_course)
 
-        return True
+        return 'Course is created'
 
-    def add_student_to_course(self,student:Student,course:Course):
-        if course.enroll_student(student, self.course_repo.get_all()):
-            selected_course = self.course_repo.get(course.id)
-            selected_course.students.add(student.id)
-            return True
+    def add_student_to_course(self, student_to_add: Student, course_to_add: Course):
+        if course_to_add.enroll_student(student_to_add, self.course_repo.get_all()):
+            selected_course = self.course_repo.get(course_to_add.id)
+            selected_course.students.add(student_to_add.id)
+            return 'Student is added'
         else:
-            return False
+            return 'Student is not added'
 
-    def add_teacher_to_course(self,teacher:Teacher,course:Course):
-        if course.assign_teacher(teacher,self.course_repo.get_all()):
-            selected_course = self.course_repo.get(course.id)
-            selected_course.teacher.add(teacher.id)
-            return True
+    def add_teacher_to_course(self, teacher_to_add: Teacher, course_to_add: Course):
+        if course_to_add.assign_teacher(teacher_to_add, self.course_repo.get_all()):
+            selected_course = self.course_repo.get(course_to_add.id)
+            selected_course.teacher.add(teacher_to_add.id)
+            return 'Teacher is added'
         else:
-            return False
+            return 'Teacher is not added'
